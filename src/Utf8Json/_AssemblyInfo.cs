3965dd3bffe3c796eb2cda923b3a6ce679accc13
xiaozg <geeking@live.cn>
2021-02-04 00:25:03 +0800

    publish nuget package 2.0.0

--- src/Utf8Json/_AssemblyInfo.cs
+++ src/Utf8Json/_AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Utf8Json")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("forked from neuecc/Utf8Json, thanks to Mr. neuecc for the greate project\r\nallow string convert to number and revert")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyTrademark("")]
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 [assembly: Guid("929e3779-4c3a-42a3-bc72-3d77511e988f")]
-[assembly: AssemblyVersion("1.3.7")]
-[assembly: AssemblyFileVersion("1.3.7")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
